2fe71e1c34840079c7f5b0de9b94ddebf2fd8413
rosibes <seby.boiciuc03@e-uvt.ro>
2025-05-05 09:33:33 +0300

    fixed prisma

--- packages/database/generated/prisma/edge.js
+++ packages/database/generated/prisma/edge.js
@@ -164,7 +164,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
