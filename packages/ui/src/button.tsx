1e3123b52725f8cc336741aa104d3b2005815443
rosibes <seby.boiciuc03@e-uvt.ro>
2025-05-05 17:53:37 +0300

    created button and input components in UI package

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,18 +1,41 @@
+// packages/ui/src/button.tsx
 "use client";
 
 import { ReactNode } from "react";
 
 interface ButtonProps {
+  variant: "primary" | "secondary";
+  size: "sm" | "md" | "lg";
   children: ReactNode;
   className?: string;
-  appName: string;
+  appName?: string;
+  onClick?: () => void;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+const variantStyle = {
+  primary: "bg-purple-600 text-white",
+  secondary: "bg-purple-100 text-purple-600",
+};
+
+const defaultStyles = "rounded-md flex cursor-pointer";
+
+const sizeStyles = {
+  sm: "py-2 px-3",
+  md: "py-3 px-5",
+  lg: "py-4 px-7",
+};
+
+export const Button = ({
+  children,
+  className = "",
+  onClick,
+  variant,
+  size,
+}: ButtonProps) => {
   return (
     <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      onClick={onClick}
+      className={`${defaultStyles} ${variantStyle[variant]} ${sizeStyles[size]} ${className}`}
     >
       {children}
     </button>
