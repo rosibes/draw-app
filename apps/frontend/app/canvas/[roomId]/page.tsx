3d4f1e099d222a3b4ad56258ac679216134672eb
rosibes <seby.boiciuc03@e-uvt.ro>
2025-05-06 14:59:24 +0300

    restructure

--- apps/frontend/app/canvas/[roomId]/page.tsx
+++ apps/frontend/app/canvas/[roomId]/page.tsx
@@ -1,49 +1,16 @@
 "use client"
+import { initDraw } from "@/draw";
 import { useEffect, useRef } from "react"
-import { start } from "repl";
 
 export default function Canvas() {
     const canvasRef = useRef<HTMLCanvasElement>(null)
 
     useEffect(() => {
         if (canvasRef.current) {
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext("2d");
-
-            if (!ctx) {
-                return;
-            }
-
-            let clicked = false;
-            let startX = 0;
-            let startY = 0;
-
-            canvas.addEventListener("mousedown", (e) => {
-                clicked = true;
-                startX = e.clientX;
-                startY = e.clientY;
-                console.log("down", e.clientY)
-            })
-
-
-            canvas.addEventListener("mouseup", (e) => {
-                clicked = false
-                console.log("up:", e.clientX)
-                console.log("up:", e.clientY)
-            })
-
-            canvas.addEventListener("mousemove", (e) => {
-                if (clicked) {
-                    const width = e.clientX - startX;
-                    const height = e.clientY - startY;
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    ctx.strokeRect(startX, startY, width, height);
-                }
-
-            })
+            initDraw(canvasRef.current)
         }
     }, [canvasRef])
     return <div>
-        <canvas ref={canvasRef} width={500} height={500}></canvas>
+        <canvas ref={canvasRef} width={2000} height={1000}></canvas>
     </div>
 }
